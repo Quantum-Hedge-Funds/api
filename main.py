819aa3aca277665d92f41ae60d5eb252c68a23a1
Yash Goyal <yashgo0018@gmail.com>
2023-12-04 10:21:17 +0530

    fixed some bugs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from quantum_optimizer import QuantumOptimizer
 from utils import calculate_similarity_matrix
@@ -21,7 +21,11 @@ app.add_middleware(
 @app.post("/run")
 async def run(data: List[List[float]] = Body(), q: int | None = Body(1)):
     n = len(data)
+    if n == 0:
+        raise HTTPException(409, {"error": "alteast one asset is required"})
     q_fix = n-q
+    if q_fix < 0:
+        raise HTTPException(400, {"error": "the required number of assets must be smaller than the total number of assets"})
     rho = calculate_similarity_matrix(data, n)
     quantum_optimizer = QuantumOptimizer(rho, n, q_fix)
     svqe_state, svqe_level = quantum_optimizer.sampling_vqe_solution()
